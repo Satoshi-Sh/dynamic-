0e92cece55eea8b380f7cf0a0788566e9a3eb191
Satoshi-Sh <satoss1108@gmail.com>
2022-09-18 17:39:07 -0500

    Complete simple dropdown menu bar
    Start work on picture slide bar next

--- dropdown/src/index.js
+++ dropdown/src/index.js
@@ -1,6 +1,39 @@
-import _ from 'lodash';
 import './style.css';
 
-const nav = document.querySelector('.navigation');
+// header
+const linksLeft = document.querySelector('.left.links');
+const linksRight = document.querySelector('.right.links');
 
-console.log(nav.innerText);
+function createLi(name, link, element) {
+  const li = document.createElement('li');
+  li.classList.add('link');
+  li.innerHTML = `<a href= ${link}>${name}</a>`;
+  li.setAttribute('id', name);
+  element.appendChild(li);
+}
+
+createLi('Home', '#', linksLeft);
+createLi('Blog', '#', linksLeft);
+createLi('Projects', '#', linksRight);
+createLi('More', '#', linksRight);
+
+// more-links
+
+const moreLinks = document.querySelector('.more-links');
+
+createLi('Kaggle', '#', moreLinks);
+createLi('Github', '#', moreLinks);
+createLi('Music Gallery', '#', moreLinks);
+
+// toggle on More
+const moreDiv = document.querySelector('.more-div');
+function toggleMore() {
+  if (moreDiv.style.display === 'none') {
+    moreDiv.style.display = 'block';
+  } else {
+    moreDiv.style.display = 'none';
+  }
+}
+
+const more = document.querySelector('#More');
+more.onclick = toggleMore;
